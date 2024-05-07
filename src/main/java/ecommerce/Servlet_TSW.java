dd1263bb1e83e24a5eee78b78557783f7439cc01
luka <l.cammarota8@studenti.unisa.it>
2024-05-07 12:56:55 +0200

    ue

---
+++ src/main/java/ecommerce/Servlet_TSW.java
@@ -0,0 +1,22 @@
+package ecommerce;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+
+public class Servlet_TSW extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  
+	    response.getWriter().println("Hello, world!");
+	}
+
+}
